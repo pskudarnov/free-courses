3524e0ec3a96e515a1314c23fa14f4ccae4cb3de
p.skudarnov <p.skudarnov@rambler-co.ru>
2018-06-09 14:17:20 +0300

    Поправил 1 задачу

--- task1.js
+++ task1.js
@@ -1,12 +1,9 @@
-function numberSystems(i,a,b) {
+var i = process.argv[2];
+var a = process.argv[3];
+var b = process.argv[4];
 
-    var x = parseInt(i, a); // I - числов перевели в 10 системе счисления
+var x = parseInt(i, a); // I - числов перевели в 10 системе счисления
 
-    var y = x.toString(b); // перевели полученое число из 10 системы в B-систему
+var y = x.toString(b); // перевели полученое число из 10 системы в B-систему
 
-    console.log(y);
-
-    return y;
-}
-
-console.log(numberSystems(process.argv[2], process.argv[3],process.argv[4]));
+console.log(y);
