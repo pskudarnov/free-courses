223ec2cfdd3c1d1d78eb9401f27d0955a812fa2a
p.skudarnov <p.skudarnov@rambler-co.ru>
2018-06-11 09:50:12 +0300

    v3.0

--- task3.js
+++ task3.js
@@ -6,23 +6,42 @@ var hour = Math.floor(t / 3600); //часы
 var minutes = Math.floor((t - (hour * 3600)) / 60);   //минуты
 var seconds = t - ((hour * 3600) + (minutes * 60)); //секунды
 
-var h = plural(hour, ['час', 'часа', 'часов']);
-var m = plural(minutes, ['минута', 'минуты', 'минут']);
-var s = plural(seconds, ['секунда', 'секунды', 'секунд']);
+if (hour === 0) console.log('');
+var i = 'часов';
+var remainder = hour % 10;
+
+if (remainder === 1) {
+    i = 'час';
+} else if (remainder > 1 && remainder < 5) {
+    i = 'часа';
+}
+
+var h = hour + ' ' + i + ' ';
+
+if (minutes === 0) console.log('');
+
+var x = 'минут';
+var remainder2 = minutes % 10;
+
+if (remainder2 === 1) {
+    x = 'минута';
+} else if (remainder2 > 1 && remainder2 < 5) {
+    x = 'минуты';
+}
+
+var m = minutes + ' ' + x + ' ';
+
+var y = 'секунда';
+var remainder3 = seconds % 10;
+
+if (remainder3 === 1) {
+    y = 'секунды';
+} else if (remainder3 > 1 && remainder3 < 5) {
+    y = 'секунд';
+}
+
+var s = seconds + ' ' + y + ' ';
+
 
 console.log(h + m + s);
 
-function plural(number, title) {
-    if (number === 0) return '';
-
-    var i = 2;
-    var remainder = number % 10;
-
-    if (remainder === 1) {
-        i = 0;
-    } else if (remainder > 1 && remainder < 5) {
-        i = 1;
-    }
-
-    return number + ' ' + title[i] + ' ';
-}
